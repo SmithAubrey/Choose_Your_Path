78ee9f1c98a3a032c59102a3c152ed0afec25a73
SmithAubrey <Smith.a201132@stu.sanjac.edu>
2026-04-21 16:59:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ button.textContent = "Are You Sure?";
 });
   
 const swordimg = document.getElementById("swordimg");
+
 swordimg.addEventListener("mouseover", () => {
-  swordimg.src = "/path2/assetpinksword2.png";
+  swordimg.src = "/path2/assetsword2.png";
 });
 swordimg.addEventListener("mouseout", () => {
   swordimg.src = "/path2/assetredsword2.png";

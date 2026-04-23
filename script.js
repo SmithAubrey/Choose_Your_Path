ed9b475345ee80a6326e60b8461b497350909626
SmithAubrey <Smith.a201132@stu.sanjac.edu>
2026-04-23 15:07:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,3 @@ const button = document.getElementById("btn");
 button.addEventListener("click", () => {
 button.textContent = "Are You Sure?";
 });
-  
-const swordimg = document.getElementById("swordimg");
-
-swordimg.addEventListener("mouseover", () => {
-  swordimg.src = "/path2/assetsword2.png";
-});
-swordimg.addEventListener("mouseout", () => {
-  swordimg.src = "/path2/assetredsword2.png";
-});
